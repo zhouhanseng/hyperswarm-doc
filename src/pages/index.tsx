57dde1f87dc6c1dc6f4ae1fad52697b8a4824f91
Hancheng Zhou <z308114274@gmail.com>
2020-09-20 12:35:42 +0800

    rm unused

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,71 +1,10 @@
-import Link from "next/link";
 import Layout from "../components/Layout";
 import { connect } from "react-redux";
 
 import { hyperstore } from "../store";
-import { useQuery, gql } from "@apollo/client";
 
-interface Props {
-  username: string;
-  namespacesRequired: string[];
-}
-
-const EXCHANGE_RATES = gql`
-  query GetExchangeRates($currency: String!) {
-    rates(currency: $currency) {
-      currency
-      rate
-      name
-    }
-  }
-`;
-
-const IndexPage = ({ username }: Props) => {
-  // const { loading, error, data } = useQuery(EXCHANGE_RATES, {
-  //   variables: { currency: "USD" },
-  //   // Doc: Pass in false to skip your query during server-side rendering.
-  //   ssr: false,
-  // });
-
-  // setTimeout(() => {
-  //   hyperstore.user.update({ name: "abddddddd" });
-  // }, 2000);
-
-  return (
-    <Layout title="Home | Next.js + TypeScript Example">
-      {/* <h1>Hello Next.js {username} 👋</h1>
-      <p>
-        <Link href="/redux">
-              <a>About{t("common.abc")}</a>
-        </Link>
-      </p>
-      <p>
-        {loading ? (
-          <p>Loading...</p>
-        ) : error ? (
-          <p>Error</p>
-        ) : (
-          data.rates.map(
-            ({
-              currency,
-              rate,
-              name,
-            }: {
-              currency: string;
-              rate: string;
-              name: string;
-            }) => (
-              <div key={currency}>
-                <p>
-                  {name} {currency}: {rate}
-                </p>
-              </div>
-            )
-          )
-        )}
-      </p> */}
-    </Layout>
-  );
+const IndexPage = () => {
+  return <Layout title="Home | Next.js + TypeScript Example"></Layout>;
 };
 
 /**
